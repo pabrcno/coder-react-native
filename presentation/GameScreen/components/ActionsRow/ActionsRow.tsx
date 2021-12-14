8d3ebf40b3726b3e7a614500e47c8d1214a5f82d
pabrcno <pabrcn@gmail.com>
2021-12-14 13:36:29 -0300

    code refactoring cellGrid

---
+++ presentation/GameScreen/components/ActionsRow/ActionsRow.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import styled from "styled-components/native";
+import { numCols, numRows } from "../../utils/constants";
+import generateEmptyGrid from "../../utils/generateEmptyGrid";
+
+const StyledButton = styled.TouchableOpacity`
+  background-color: #202020;
+  padding: 20px;
+  border-radius: 2px;
+  align-items: center;
+  justify-content: center;
+  width: 120px;
+`;
+
+interface IProps {
+  toggleRunning: () => void;
+  running: boolean;
+  setGrid: (grid: number[][]) => void;
+}
+export const ActionsRow = ({ toggleRunning, running, setGrid }: IProps) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        backgroundColor: "#202020",
+        paddingBottom: 80,
+      }}
+    >
+      <StyledButton onPress={toggleRunning}>
+        <Text style={styles.text}>{running ? "STOP" : "START"}</Text>
+      </StyledButton>
+
+      <StyledButton
+        onPress={() => {
+          const rows = [];
+          for (let i = 0; i < numRows; i++) {
+            rows.push(
+              Array.from(Array(numCols), () => (Math.random() > 0.8 ? 1 : 0))
+            );
+          }
+
+          setGrid(rows);
+        }}
+      >
+        <Text style={styles.text}>Random</Text>
+      </StyledButton>
+      <StyledButton
+        onPress={() => {
+          setGrid(generateEmptyGrid());
+        }}
+      >
+        <Text style={styles.text}>Clear</Text>
+      </StyledButton>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "VCR",
+    color: "#fff",
+    fontSize: 20,
+  },
+});
+
+export default ActionsRow;
--- presentation/GameScreen/components/CellGrid/CellGrid.tsx
+++ presentation/GameScreen/components/CellGrid/CellGrid.tsx
@@ -11,38 +11,13 @@ import {
 } from "react-native";
 import styled from "styled-components/native";
 import { Row } from "react-native-easy-grid";
-
-const numRows = 25;
-const numCols = 16;
-
-const operations = [
-  [0, 1],
-  [0, -1],
-  [1, -1],
-  [-1, 1],
-  [1, 1],
-  [-1, -1],
-  [1, 0],
-  [-1, 0],
-];
+import Cell from "../Cell";
+import { numCols, numRows, operations } from "../../utils/constants";
+import generateEmptyGrid from "../../utils/generateEmptyGrid";
+import { ActionsRow } from "./../ActionsRow/ActionsRow";
+import gameLogic from "../../utils/gameLogic";
 
 const StyledGrid = styled.View``;
-const StyledButton = styled.TouchableOpacity`
-  background-color: #202020;
-  padding: 20px;
-  border-radius: 2px;
-  align-items: center;
-  justify-content: center;
-  width: 120px;
-`;
-const generateEmptyGrid = () => {
-  const rows = [];
-  for (let i = 0; i < numRows; i++) {
-    rows.push(Array.from(Array(numCols), () => 0));
-  }
-
-  return rows;
-};
 
 const CellGrid = () => {
   const [grid, setGrid] = useState(() => {
@@ -58,29 +33,7 @@ const CellGrid = () => {
     if (!runningRef.current) {
       return;
     }
-
-    setGrid((g) => {
-      return produce(g, (gridCopy) => {
-        for (let i = 0; i < numRows; i++) {
-          for (let k = 0; k < numCols; k++) {
-            let neighbors = 0;
-            operations.forEach(([x, y]) => {
-              const newI = i + x;
-              const newK = k + y;
-              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
-                neighbors += g[newI][newK];
-              }
-            });
-
-            if (neighbors < 2 || neighbors > 3) {
-              gridCopy[i][k] = 0;
-            } else if (g[i][k] === 0 && neighbors === 3) {
-              gridCopy[i][k] = 1;
-            }
-          }
-        }
-      });
-    });
+    setGrid(gameLogic);
 
     setTimeout(runSimulation, 150);
   }, []);
@@ -91,7 +44,7 @@ const CellGrid = () => {
         {grid.map((rows, i) => (
           <Row key={`${i}`}>
             {rows.map((col, k) => (
-              <TouchableWithoutFeedback
+              <Cell
                 key={`${i}-${k}`}
                 onPress={() => {
                   const newGrid = produce(grid, (gridCopy) => {
@@ -99,64 +52,25 @@ const CellGrid = () => {
                   });
                   setGrid(newGrid);
                 }}
-              >
-                <View
-                  style={{
-                    width: 24,
-                    height: 24,
-                    borderWidth: 0.5,
-                    borderColor: "black",
-                    backgroundColor: grid[i][k] ? "black" : "white",
-                  }}
-                />
-              </TouchableWithoutFeedback>
+                i={i}
+                k={k}
+                grid={grid}
+              />
             ))}
           </Row>
         ))}
       </StyledGrid>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-around",
-          backgroundColor: "#202020",
-          paddingBottom: 80,
+      <ActionsRow
+        toggleRunning={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
         }}
-      >
-        <StyledButton
-          onPress={() => {
-            setRunning(!running);
-            if (!running) {
-              runningRef.current = true;
-              runSimulation();
-            }
-          }}
-        >
-          <Text style={styles.text}>{running ? "STOP" : "START"}</Text>
-        </StyledButton>
-
-        <StyledButton
-          onPress={() => {
-            const rows = [];
-            for (let i = 0; i < numRows; i++) {
-              rows.push(
-                Array.from(Array(numCols), () => (Math.random() > 0.5 ? 1 : 0))
-              );
-            }
-
-            setGrid(rows);
-          }}
-        >
-          <Text style={styles.text}>Random</Text>
-        </StyledButton>
-        <StyledButton
-          onPress={() => {
-            setGrid(generateEmptyGrid());
-          }}
-        >
-          <Text style={styles.text}>Clear</Text>
-        </StyledButton>
-      </View>
+        running={running}
+        setGrid={setGrid}
+      />
     </ScrollView>
   );
 };
